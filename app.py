d40008921ca27424426d68ae8c8ae935e4d9f14f
kodchapan <51810277+kodchapan@users.noreply.github.com>
2019-07-19 11:04:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def callback():
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-        if (userText == 'สวัสดี') :
+    if (userText == 'สวัสดี') :
         sendText(user,'เซมจ่ะ')
     elif (userText == 'ไปละ') :
        sendText(user,'จะไปไหนก็ไป')
